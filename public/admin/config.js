4722a3a3c0565e1168659cfc11809bda54bbb4aa
Laurent Chedanne <laurent@chedanne.pro>
2025-12-30 18:58:02 +0100

    Update config.js
    Fix base_url

--- public/admin/config.js
+++ public/admin/config.js
@@ -7,7 +7,7 @@ CMS.init({
       name: "github",
       repo: "lolozere/arbraphore",
       branch: "main",
-      base_url: "https://arbraphore-auth.lolozere.workers.dev"
+      base_url: "https://arabraphore-decap-auth.laurent-a35.workers.dev"
     },
     publish_mode: "editorial_workflow",
     media_folder: "public/uploads",
